d72fbac2b4cc8cfe60a6f6e6368f8487f3977fbc
Ankit Kathayat <93024110+iamankitkathayat@users.noreply.github.com>
2022-10-20 23:01:40 +0530

    Update KingJames.sol

--- contracts/Tokens/KingJames.sol
+++ contracts/Tokens/KingJames.sol
@@ -1,1 +1,21 @@
+// SPDX-License-Identifier: MIT
 
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract KingJames is ERC20 {
+
+    address public _owner;
+
+    uint256 private _totalSupply = 90000000000000000000000000; //90,000,000 KingJames tokens
+
+    mapping(address => uint256) private _balances;
+    
+    constructor() ERC20("KingJames token", "KingJames") {
+        _owner = msg.sender;
+        _mint(msg.sender, _totalSupply);
+        _balances[_owner] = _totalSupply;
+    }
+
+}
