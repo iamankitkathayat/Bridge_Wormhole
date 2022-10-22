12ca5c923e9d2ddfbda6acb53c7a7fa9f2d04bfd
Ankit Kathayat <93024110+iamankitkathayat@users.noreply.github.com>
2022-10-22 23:04:45 +0530

    Update deployKingJames.js

--- scripts/deployKingJames.js
+++ scripts/deployKingJames.js
@@ -1,1 +1,19 @@
+const hre = require("hardhat");
 
+async function main() {
+
+  const KingJamestoken = await hre.ethers.getContractFactory("KingJames");
+  const kj = await KingJamestoken.deploy();
+
+  await kj.deployed();
+  
+  console.log("KingJames token deployed to ", kj.address);
+}
+
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
